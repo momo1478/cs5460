f590457393f2e4a29f9ed7571d0cf57ea9f8e1ff
momo1478 <monish1478@gmail.com>
2018-02-06 11:59:05 -0700

    A2 stuff

---
+++ a2/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/student/Documents/A2/hello.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-45-generic/scripts/module-common.lds --build-id  -o /home/student/Documents/A2/hello.ko /home/student/Documents/A2/hello.o /home/student/Documents/A2/hello.mod.o
---
+++ a2/.tmp_versions/hello.mod
@@ -0,0 +1,2 @@
+/home/student/Documents/A2/hello.ko
+/home/student/Documents/A2/hello.o
