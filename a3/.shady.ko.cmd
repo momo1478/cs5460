a957ab23e7800e5ed08308a894ee92981e5adeb5
Your Name <you@example.com>
2018-03-09 20:25:49 -0700

    add a3

---
+++ a3/.shady.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/student/Desktop/assignment3/shady.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-45-generic/scripts/module-common.lds --build-id  -o /home/student/Desktop/assignment3/shady.ko /home/student/Desktop/assignment3/shady.o /home/student/Desktop/assignment3/shady.mod.o
---
+++ a3/shady.mod.c
@@ -0,0 +1,46 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x9412fa01, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xb6b46a7c, __VMLINUX_SYMBOL_STR(param_ops_int) },
+	{ 0xda6982e5, __VMLINUX_SYMBOL_STR(device_create) },
+	{ 0x987fde2e, __VMLINUX_SYMBOL_STR(cdev_add) },
+	{ 0xb46aca86, __VMLINUX_SYMBOL_STR(cdev_init) },
+	{ 0xac7f6dfd, __VMLINUX_SYMBOL_STR(__mutex_init) },
+	{ 0xd2b09ce5, __VMLINUX_SYMBOL_STR(__kmalloc) },
+	{ 0xd7bd463f, __VMLINUX_SYMBOL_STR(__class_create) },
+	{ 0x29537c9e, __VMLINUX_SYMBOL_STR(alloc_chrdev_region) },
+	{ 0x7485e15e, __VMLINUX_SYMBOL_STR(unregister_chrdev_region) },
+	{ 0x268f87ce, __VMLINUX_SYMBOL_STR(class_destroy) },
+	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
+	{ 0xd743757b, __VMLINUX_SYMBOL_STR(cdev_del) },
+	{ 0xc9296d71, __VMLINUX_SYMBOL_STR(device_destroy) },
+	{ 0x1eecf3fc, __VMLINUX_SYMBOL_STR(mutex_unlock) },
+	{ 0x5f6629de, __VMLINUX_SYMBOL_STR(mutex_lock_killable) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "72D793380CD3B31807DE9E3");
